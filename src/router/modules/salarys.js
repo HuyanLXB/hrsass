9856ac26a72af1543cd664ed293765678358eb55
HUYANLXB <lvxubo2000@foxmail.com>
2023-02-20 19:06:41 +0800

    '完成左侧导航栏图标的展示'

--- src/router/modules/salarys.js
+++ src/router/modules/salarys.js
@@ -13,7 +13,8 @@ export default {
     meta: {
       // meta属性的里面的属性 随意定义
       // 但是这里为什么要用title呢， 因为左侧导航会读取我们的路由里的meta里面的title作为显示菜单名称
-      title: '工资'
+      title: '工资',
+      icon: 'money'
     }
   }]
 }
