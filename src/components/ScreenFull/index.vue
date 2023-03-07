1fe0f6dac0d527dffbc4030fd087cf0c6e5ed13a
HUYANLXB <lvxubo2000@foxmail.com>
2023-03-07 09:39:00 +0800

    '完成全屏切换组件的封装'

---
+++ src/components/ScreenFull/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <svg-icon
+      icon-class="fullscreen"
+      style="color:#fff; width: 20px; height: 20px"
+      @click="changeScreen"
+    />
+  </div>
+
+</template>
+
+<script>
+import ScreenFull from 'screenfull'
+export default {
+  name: 'ScreenFull',
+  methods: {
+    changeScreen() {
+      // 切换全屏
+      if (!ScreenFull.isEnabled) {
+        // 表示全屏不可用
+        this.$message('全屏功能暂不可用')
+        return
+      }
+      ScreenFull.toggle()
+    }
+
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,13 @@
 import PageToll from '@/components/PageTools'
 import UploadExcel from '@/components/UploadExcel'
 import ImgUpload from '@/components/ImageUpload'
+import ScreenFull from '@/components/ScreenFull'
+
 export default {
   install(Vue) {
     Vue.component('PageToll', PageToll) // 全局注册一个工具栏
     Vue.component('UploadExcel', UploadExcel) // 全局注册一个Excel导入组件
     Vue.component('ImgUpload', ImgUpload) // 全局注册一个图片上传组件
+    Vue.component('ScreenFull', ScreenFull) // 全局注册一个图片上传组件
   }
 }
