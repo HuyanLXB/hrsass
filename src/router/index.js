0b162cf580c68f5ca8cb5b5ffec40a35062ee994
HUYANLXB <lvxubo2000@foxmail.com>
2023-03-07 14:25:39 +0800

    '将路由模式改为history'

--- src/router/index.js
+++ src/router/index.js
@@ -92,7 +92,8 @@ export const asyncRoutes = [
 ]
 
 const createRouter = () => new Router({
-  // mode: 'history', // require service support
+  mode: 'history', // require service support
+  base: 'hr',
   scrollBehavior: () => ({ y: 0 }),
   // 将动态路由和静态路由进行临时合并
   routes: [...constantRoutes]
